470d4ca920fa3b545d386269f4fe2bbee547d838
Ranjeet Singh <44586579+jeetech77@users.noreply.github.com>
2021-05-22 12:09:53 +0530

    Added URL

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-var inputArea=document.querySelector(".input-text");
-var translateBtn=document.querySelector(".btn");
-var outputArea=document.querySelector(".output-text");
-translateBtn.addEventListener('click',()=>{
-    var inputText=inputArea.value;
-    outputArea.innerHTML=inputText;
-    inputArea.style.color.green;
+var inputArea = document.querySelector(".input-text");
+var translateBtn = document.querySelector(".btn");
+var outputArea = document.querySelector(".output-text");
+var url = "	https://api.funtranslations.com/translate/pirate.json";
+translateBtn.addEventListener('click', () => {
+    var inputText = inputArea.value;
+    finalURL=constructURL(inputArea);
+    fetch(finalURL).then(Response=>Response.json)
+    .then(json=>{
+        translatedText=josn.content.translated;
+        outputArea.innerHTML=translatedText;
+    })
 })
+function constructURL(text) {
+    return url + "?text=" + text;
+}
