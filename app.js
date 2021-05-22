8317ce4b43f27b90d4cb2aa1975ec15c61b2fac5
Ranjeet Singh <44586579+jeetech77@users.noreply.github.com>
2021-05-22 20:02:35 +0530

    Finished the project

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ var inputArea = document.querySelector(".input-text");
 var translateBtn = document.querySelector(".btn");
 var outputArea = document.querySelector(".output-text");
 var url = "	https://api.funtranslations.com/translate/pirate.json";
+function constructURL(text) {
+    return (url + "?text=" + text);
+}
 translateBtn.addEventListener('click', () => {
     var inputText = inputArea.value;
-    finalURL=constructURL(inputArea);
-    fetch(finalURL).then(Response=>Response.json)
+    finalURL=constructURL(inputText);
+    fetch(finalURL).then(Response=>Response.json())
     .then(json=>{
-        translatedText=josn.content.translated;
+        translatedText=json.contents.translated;
         outputArea.innerHTML=translatedText;
     })
 })
-function constructURL(text) {
-    return url + "?text=" + text;
-}
